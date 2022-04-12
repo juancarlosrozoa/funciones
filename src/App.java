691358231cde5fca6fbb55bf54f452efe1063fb5
juancarlosrozoa <juancarlosrozoanaya@gmail.com>
2022-04-12 09:50:38 -0500

    Actividad funciones OpenBootcamp

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+      //Llamar la funcion suma Primera parte
+        int resultado;
+       resultado = suma(10,20,30);
+        System.out.println(resultado);
+    }
+
+    // Funcion sumar tres valores primera parte
+    public static int suma (int a, int b, int c){
+        return a+b+c;
+    }
+}
+
